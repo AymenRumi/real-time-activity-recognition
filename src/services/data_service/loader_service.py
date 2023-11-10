39c577a6e1d2fba529099d21df37f3bb597f5b1c
Aymen Rumi <aymen.rumi@mail.mcgill.ca>
2023-11-09 21:00:31 -0500

    label mapping import as constant

--- src/services/data_service/loader_service.py
+++ src/services/data_service/loader_service.py
@@ -4,6 +4,8 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset
 
+from src.constants import label_mapping
+
 DIRECTORY_PATH = "src/data"
 
 
@@ -12,20 +14,11 @@ class CustomDataset(Dataset):
         self.data_files = []
         self.labels = []
 
-        self.label_mapping = {
-            "LAYING": 0,
-            "SITTING": 1,
-            "STANDING": 2,
-            "WALKING": 3,
-            "WALKING_DOWNSTAIRS": 4,
-            "WALKING_UPSTAIRS": 5,
-        }
-
         for label_folder in os.listdir(directory):
             label_path = os.path.join(directory, label_folder)
             if os.path.isdir(label_path):
 
-                label = self.label_mapping[label_folder]
+                label = label_mapping[label_folder]
                 for file in os.listdir(label_path):
                     if file.endswith(".csv"):
                         file_path = os.path.join(label_path, file)

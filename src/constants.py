81db202013d7011c658d31b2640f7adf9f65f4e4
Aymen Rumi <aymen.rumi@mail.mcgill.ca>
2023-11-09 20:59:01 -0500

    constants

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+label_mapping = {
+    "LAYING": 0,
+    "SITTING": 1,
+    "STANDING": 2,
+    "WALKING": 3,
+    "WALKING_DOWNSTAIRS": 4,
+    "WALKING_UPSTAIRS": 5,
+}
+
+index_mapping = {v: k for k, v in label_mapping.items()}
